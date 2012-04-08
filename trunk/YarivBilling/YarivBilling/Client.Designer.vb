b4fbb75a6f3900a79b6af072c92e80e0f2f89af5
shakedav@gmail.com <shakedav@gmail.com@f44e9078-5545-6cde-34e1-2dc76b7ce89f>
2012-04-08 19:59:59 +0000

    Save to excel

---
+++ trunk/YarivBilling/YarivBilling/Client.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Client
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtBoxName = New System.Windows.Forms.TextBox()
+        Me.txtBoxProjectNumber = New System.Windows.Forms.TextBox()
+        Me.txtBoxAddress = New System.Windows.Forms.TextBox()
+        Me.txtBoxPhone = New System.Windows.Forms.TextBox()
+        Me.txtBoxEmail = New System.Windows.Forms.TextBox()
+        Me.txtBoxContactMan = New System.Windows.Forms.TextBox()
+        Me.txtBoxClientNumber = New System.Windows.Forms.TextBox()
+        Me.txtBoxProjectNameInviter = New System.Windows.Forms.TextBox()
+        Me.txtBoxProjectCodeInviter = New System.Windows.Forms.TextBox()
+        Me.txtBoxProjectName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "שם לקוח:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "כתובת:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "טלפון:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 165)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "קוד לקוח:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 140)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "איש קשר בפרוייקט:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 115)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "אימייל:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 39)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "קוד פרוייקט:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 217)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(138, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "קוד פרוייקט אצל המזמין:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 191)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(142, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "שם הפרוייקט אצל המזמין:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 242)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(78, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "שם הפרוייקט:"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(12, 286)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 20
+        Me.btnSave.Text = "שמור"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 286)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "בטל"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtBoxName
+        '
+        Me.txtBoxName.Location = New System.Drawing.Point(73, 10)
+        Me.txtBoxName.Name = "txtBoxName"
+        Me.txtBoxName.Size = New System.Drawing.Size(187, 20)
+        Me.txtBoxName.TabIndex = 22
+        '
+        'txtBoxProjectNumber
+        '
+        Me.txtBoxProjectNumber.Location = New System.Drawing.Point(93, 36)
+        Me.txtBoxProjectNumber.Name = "txtBoxProjectNumber"
+        Me.txtBoxProjectNumber.Size = New System.Drawing.Size(167, 20)
+        Me.txtBoxProjectNumber.TabIndex = 23
+        '
+        'txtBoxAddress
+        '
+        Me.txtBoxAddress.Location = New System.Drawing.Point(93, 61)
+        Me.txtBoxAddress.Name = "txtBoxAddress"
+        Me.txtBoxAddress.Size = New System.Drawing.Size(167, 20)
+        Me.txtBoxAddress.TabIndex = 24
+        '
+        'txtBoxPhone
+        '
+        Me.txtBoxPhone.Location = New System.Drawing.Point(93, 86)
+        Me.txtBoxPhone.Name = "txtBoxPhone"
+        Me.txtBoxPhone.Size = New System.Drawing.Size(167, 20)
+        Me.txtBoxPhone.TabIndex = 25
+        '
+        'txtBoxEmail
+        '
+        Me.txtBoxEmail.Location = New System.Drawing.Point(93, 112)
+        Me.txtBoxEmail.Name = "txtBoxEmail"
+        Me.txtBoxEmail.Size = New System.Drawing.Size(167, 20)
+        Me.txtBoxEmail.TabIndex = 26
+        '
+        'txtBoxContactMan
+        '
+        Me.txtBoxContactMan.Location = New System.Drawing.Point(127, 137)
+        Me.txtBoxContactMan.Name = "txtBoxContactMan"
+        Me.txtBoxContactMan.Size = New System.Drawing.Size(133, 20)
+        Me.txtBoxContactMan.TabIndex = 27
+        '
+        'txtBoxClientNumber
+        '
+        Me.txtBoxClientNumber.Location = New System.Drawing.Point(76, 162)
+        Me.txtBoxClientNumber.Name = "txtBoxClientNumber"
+        Me.txtBoxClientNumber.Size = New System.Drawing.Size(184, 20)
+        Me.txtBoxClientNumber.TabIndex = 28
+        '
+        'txtBoxProjectNameInviter
+        '
+        Me.txtBoxProjectNameInviter.Location = New System.Drawing.Point(160, 188)
+        Me.txtBoxProjectNameInviter.Name = "txtBoxProjectNameInviter"
+        Me.txtBoxProjectNameInviter.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxProjectNameInviter.TabIndex = 29
+        '
+        'txtBoxProjectCodeInviter
+        '
+        Me.txtBoxProjectCodeInviter.Location = New System.Drawing.Point(156, 214)
+        Me.txtBoxProjectCodeInviter.Name = "txtBoxProjectCodeInviter"
+        Me.txtBoxProjectCodeInviter.Size = New System.Drawing.Size(104, 20)
+        Me.txtBoxProjectCodeInviter.TabIndex = 30
+        '
+        'txtBoxProjectName
+        '
+        Me.txtBoxProjectName.Location = New System.Drawing.Point(93, 240)
+        Me.txtBoxProjectName.Name = "txtBoxProjectName"
+        Me.txtBoxProjectName.Size = New System.Drawing.Size(167, 20)
+        Me.txtBoxProjectName.TabIndex = 31
+        '
+        'Client
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(318, 324)
+        Me.Controls.Add(Me.txtBoxProjectName)
+        Me.Controls.Add(Me.txtBoxProjectCodeInviter)
+        Me.Controls.Add(Me.txtBoxProjectNameInviter)
+        Me.Controls.Add(Me.txtBoxClientNumber)
+        Me.Controls.Add(Me.txtBoxContactMan)
+        Me.Controls.Add(Me.txtBoxEmail)
+        Me.Controls.Add(Me.txtBoxPhone)
+        Me.Controls.Add(Me.txtBoxAddress)
+        Me.Controls.Add(Me.txtBoxProjectNumber)
+        Me.Controls.Add(Me.txtBoxName)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Client"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.RightToLeftLayout = True
+        Me.Text = "לקוח"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents txtBoxName As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxProjectNumber As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxPhone As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxEmail As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxContactMan As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxClientNumber As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxProjectNameInviter As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxProjectCodeInviter As System.Windows.Forms.TextBox
+    Friend WithEvents txtBoxProjectName As System.Windows.Forms.TextBox
+
+End Class
---
+++ trunk/YarivBilling/YarivBilling/Client.vb
@@ -0,0 +1,95 @@
+﻿Imports Microsoft.Office.Interop
+Imports Microsoft.Office.Interop.Excel
+
+Public Class Client
+
+    Dim xlApp As Excel.Application
+    Dim wb As Workbook
+    Dim ws As Worksheet
+    Dim var As String
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+        Dim xlApp As Excel.Application
+        Dim xlWorkBook As Excel.Workbook
+        Dim xlWorkSheet As Excel.Worksheet
+        xlApp = New Excel.Application
+        If Not (FileInUse("E:\Yariv Handasa\YarivBilling\DataBase.xls")) Then
+            xlWorkBook = xlApp.Workbooks.Open("E:\Yariv Handasa\YarivBilling\DataBase.xls", , False, , , , True, , , True, , , , , )
+            xlWorkSheet = xlWorkBook.Sheets("לקוחות")
+            Dim items(0 To 10) As String
+            items(0) = txtBoxName.Text
+            items(1) = txtBoxProjectNumber.Text
+            items(2) = txtBoxAddress.Text
+            items(3) = txtBoxPhone.Text
+            items(4) = txtBoxEmail.Text
+            items(5) = txtBoxContactMan.Text
+            items(6) = txtBoxClientNumber.Text
+            items(7) = txtBoxProjectNameInviter.Text
+            items(8) = txtBoxProjectCodeInviter.Text
+            items(9) = txtBoxProjectName.Text
+            Dim column, row As Integer
+            column = 1
+            row = 2
+            'row = xlApp.Cells.Find("*", SearchOrder:=xlByRows, SearchDirection:=xlPrevious).Row
+            'If (IsNewClient(txtBoxClientNumber.Text, xlWorkSheet)) Then
+            For Each item As String In items
+                xlWorkSheet.Cells(row, column) = item
+                column = column + 1
+            Next
+            'Else
+            '    MsgBox("aaa")
+
+            'End If
+
+            xlApp.DisplayAlerts = False
+            xlWorkSheet.SaveAs("E:\Yariv Handasa\YarivBilling\DataBase.xls")
+            xlWorkBook.Close()
+            xlApp.Quit()
+            releaseObject(xlApp)
+            releaseObject(xlWorkBook)
+            releaseObject(xlWorkSheet)
+        Else
+            MsgBox("File is open, close to continue")
+        End If
+    End Sub
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+
+   Public Function FileInUse(ByVal sFile As String) As Boolean
+        If System.IO.File.Exists(sFile) Then
+            Try
+                Dim F As Short = FreeFile()
+                FileOpen(F, sFile, OpenMode.Binary, OpenAccess.ReadWrite, OpenShare.LockReadWrite)
+                FileClose(F)
+            Catch
+                Return True
+            End Try
+        End If
+    End Function
+
+
+    'Private Function IsNewClient(ByVal item As String, ByVal worksheet As Excel.Worksheet) As Boolean
+    '    Excel.Range allCellsRng;
+    'string lowerRightCell = "IV65536";
+    'allCellsRng = ws.get_Range("A1", lowerRightCell).Cells;
+    '    Dim row = 2
+    '    For Each Cell In Activeshee
+    '        If worksheet.Cells(row,0) == item Then
+    '            Return True
+    '            row = row + 1
+    '        End If
+    '    Next
+    '    Return False
+    'End Function
+
+End Class
+
